99535c311f904539a473de1705a53d8c52d315eb
Zaeema Anwar <55206089+thezaeemaanwar@users.noreply.github.com>
2020-05-03 17:37:26 +0500

    Add files via upload

---
+++ signup.php
@@ -0,0 +1,7 @@
+<?php
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $mother_tongue = $_POST['mother_tongue'];
+    $age = $_POST['age'];
+    $listner_type = $_POST['listner_type'];
+?>
